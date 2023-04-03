0c3fb7886b4768ebbbdbab6733e468ea3bd8bb77
ian <ian.orieko@student.moringaschool.com>
2023-04-03 05:34:18 +0300

    Completed assignment

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,5 @@
 class ItemsController < ApplicationController
+    def index
+        render json: Item.all, except: [:user_id, :created_at, :updated_at], include: :user
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,5 @@
 class UsersController < ApplicationController
+    def show
+        render json: User.find(params[:id]), except: [:created_at, :updated_at], include: :items
+    end
 end
